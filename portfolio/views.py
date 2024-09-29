4e7c0a9db64dae4cb72bc9420fbb54ee5b6b3552
benmulligan4NU <c1006420@newcastle.ac.uk>
2024-09-29 21:46:33 +0100

    init portfolio views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the Programs app!")
+    return render(request, 'portfolio/index.html')
